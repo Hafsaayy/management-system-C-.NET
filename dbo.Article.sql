a6e01585373a8a3b5ce7115da9d09adfd9ce9eb3
Hafsa Naeem <52010329+Hafsaayy@users.noreply.github.com>
2022-06-10 03:32:16 +0500

    Add files via upload

---
+++ dbo.Article.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Article] (
+    [Art_id]              INT            IDENTITY (1, 1) NOT NULL,
+    [Res_id]              INT            NOT NULL,
+    [Article_Heading]     NVARCHAR (100) NOT NULL,
+    [Article_discription] NVARCHAR (500) NOT NULL,
+    [File] VARBINARY(MAX) not NULL, 
+    PRIMARY KEY CLUSTERED ([Art_id] ASC),
+    CONSTRAINT [FK_Res_id] FOREIGN KEY ([Res_id]) REFERENCES [dbo].[Researcher] ([Res_id])
+);
+
